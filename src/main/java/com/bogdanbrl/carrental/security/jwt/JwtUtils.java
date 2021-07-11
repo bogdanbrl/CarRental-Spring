63c49f6088c83caff43ba164a637569d3efc185c
Bogdan Brl <bogdan.brlx@gmail.com>
2021-07-11 13:15:04 +0300

    updated secret key app prop

--- src/main/java/com/bogdanbrl/carrental/security/jwt/JwtUtils.java
+++ src/main/java/com/bogdanbrl/carrental/security/jwt/JwtUtils.java
@@ -14,10 +14,10 @@ import java.util.Date;
 public class JwtUtils {
 	private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
-	@Value("${bezkoder.app.jwtSecret}")
+	@Value("${carRental.app.jwtSecret}")
 	private String jwtSecret;
 
-	@Value("${bezkoder.app.jwtExpirationMs}")
+	@Value("${carRental.app.jwtExpirationMs}")
 	private int jwtExpirationMs;
 
 	public String generateJwtToken(Authentication authentication) {
